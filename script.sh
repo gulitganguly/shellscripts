3c883be2b8078392689870578d5c16e92d524878
Agniva De Sarker <agnivade@yahoo.co.in>
2017-07-18 15:05:27 +0530

    Changed the speed text

--- script.sh
+++ script.sh
@@ -6,6 +6,6 @@ for index in `seq 1 $NUM_CPUS`
 do
 	echo "CPU- $index"
 	cat /proc/cpuinfo | grep "model name" | head -$index | tail -1
-	cat /proc/cpuinfo | grep "cpu MHz" | head -$index | tail -1
+	cat /proc/cpuinfo | grep "cpu speed in MHz" | head -$index | tail -1
 	cat /proc/cpuinfo | grep "cache size" | head -$index | tail -1
 done
