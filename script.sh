bed855bd26b9b94595239d30b407e7c895853a38
Ayan <ayan.ganguly89@gmail.com>
2017-07-18 14:57:40 +0530

    Adding a script which reads from /proc/cpuinfo

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUM_CPUS=`cat /proc/cpuinfo | grep "model name" | wc -l`
+
+for index in `seq 1 $NUM_CPUS`
+do
+	echo "CPU- $index"
+	cat /proc/cpuinfo | grep "model name" | head -$index | tail -1
+	cat /proc/cpuinfo | grep "cpu MHz" | head -$index | tail -1
+	cat /proc/cpuinfo | grep "cache size" | head -$index | tail -1
+done
